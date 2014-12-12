d48842c5c946aeb7b18427cb294d00e47d7d637c
natio <paolo.coronati@gmail.com>
2014-12-12 17:07:28 +0100

    added share activity

--- app/src/main/java/com/gcw/sapienza/places/PlacesApplication.java
+++ app/src/main/java/com/gcw/sapienza/places/PlacesApplication.java
@@ -1,6 +1,10 @@
 package com.gcw.sapienza.places;
 
 import android.app.Application;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.cgw.sapienza.places.model.Flag;
@@ -18,15 +22,59 @@ public class PlacesApplication extends Application{
     //Parse.com client key
     private static final String PARSE_COM_CLIENT_KEY = "Gr1g8Z2kfv3AOZqToZ30hyMyNzH24vj4yudNoKfb";
 
+    //Shared location manager
+    //fixme find a better way to handle gps
+    private LocationManager locationManager;
+
+    //current location
+    private Location currentLocation = null;
+
     //method called when the app is launched
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //initialize the location manager
+        this.initLocationManager();
 
         // initialize Parse.com
         ParseObject.registerSubclass(Flag.class);
         Parse.initialize(this, PARSE_COM_APP_KEY , PARSE_COM_CLIENT_KEY);
 
         Log.d(TAG, "Hi");
+
+    }
+
+    private void initLocationManager(){
+        this.locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                PlacesApplication.this.currentLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        this.locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+    }
+
+    public Location getCurrentLocation(){
+        return this.currentLocation;
     }
 }
