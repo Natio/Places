9eb51fa5d45f9ff0b97cfd76eb0c216a006a8ef0
natio <paolo.coronati@gmail.com>
2014-12-14 20:16:38 +0100

    added RemoteSettings

---
+++ app/src/main/java/com/cgw/sapienza/palces/remotesettings/RemoteSettingsCallBacks.java
@@ -0,0 +1,18 @@
+package com.cgw.sapienza.palces.remotesettings;
+
+
+/**
+ * Interface for handling RemoteSettings configuration callbacks
+ */
+public interface RemoteSettingsCallBacks{
+    /**
+     * fired when the configuration is read from an URL
+     */
+    public void onRemoteConfig();
+
+    /**
+     * Fired when an error occurs
+     * @param error a string representing the error
+     */
+    public void onError(String error);
+}
