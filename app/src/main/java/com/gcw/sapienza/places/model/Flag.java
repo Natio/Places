2aab004cb32a913c6797260745f3ab6cfce05e44
Daniele Riccardelli <gotikk@hotmail.it>
2015-01-05 04:20:46 +0100

    removed unused code

--- app/src/main/java/com/gcw/sapienza/places/model/Flag.java
+++ app/src/main/java/com/gcw/sapienza/places/model/Flag.java
@@ -32,21 +32,6 @@ public class Flag extends ParseObject{
 
     public Date getDate() { return (Date)this.getCreatedAt(); }
 
-<<<<<<< HEAD
-    /**
-     *
-     * @return Integer.MAX_VALUE if there is no temperature
-     */
-    public int getTemp() {
-        Integer temp =(Integer) this.get("temp");
-        if(temp == null){
-            return Integer.MAX_VALUE;
-        }
-        return temp;
-    }
-
-=======
->>>>>>> 8786bf2bf9f6655c84ba91772771fad751df6c56
     public byte[] getPic() { return (byte[])this.get("pic"); }
 
     public String getWeather() { return (String)this.get("weather");  }
