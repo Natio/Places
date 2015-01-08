92b9bcfd92282408931a3dd2b9abb35865a0cb13
natio <paolo.coronati@gmail.com>
2015-01-08 19:56:50 +0100

    now it show version numbers in settings

--- app/src/main/java/com/gcw/sapienza/places/SettingsFragment.java
+++ app/src/main/java/com/gcw/sapienza/places/SettingsFragment.java
@@ -8,7 +8,7 @@ import android.preference.PreferenceFragment;
  * Created by Simone on 12/30/2014.
  */
 public class SettingsFragment extends PreferenceFragment {
-
+    @SuppressWarnings("unused")
     private static final String TAG = "SettingsFragment";
 
     @Override
@@ -18,13 +18,18 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.layout.settings_layout);
 
-        Preference loneWolf_filter = (Preference) findPreference("meFilter");
-        Preference fl_filter = (Preference) findPreference("flFilter");
-        Preference strangers_filter = (Preference) findPreference("strangersFilter");
-        Preference time_filter = (Preference) findPreference("timeFilter");
-        Preference seek_bar = (Preference) findPreference("seekBar");
-        Preference max_fetch = (Preference) findPreference("maxFetch");
+        Preference loneWolf_filter =  findPreference("meFilter");
+        Preference fl_filter = findPreference("flFilter");
+        Preference strangers_filter = findPreference("strangersFilter");
+        Preference time_filter = findPreference("timeFilter");
+        Preference seek_bar = findPreference("seekBar");
+        Preference max_fetch = findPreference("maxFetch");
+        Preference version_label = findPreference("version");
 
+        String version_name = BuildConfig.VERSION_NAME;
+        int version_code = BuildConfig.VERSION_CODE;
+
+        version_label.setSummary(version_name + " ("+version_code+")");
         loneWolf_filter.setOnPreferenceChangeListener((Preference.OnPreferenceChangeListener)getActivity());
         fl_filter.setOnPreferenceChangeListener((Preference.OnPreferenceChangeListener)getActivity());
         strangers_filter.setOnPreferenceChangeListener((Preference.OnPreferenceChangeListener)getActivity());
