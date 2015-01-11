c1a57925da1873330358edfb4c2e983f8714cc28
natio <paolo.coronati@gmail.com>
2015-01-11 20:33:50 +0100

    solved a bug thay caused all Flag instance to return true on isDirty()

--- app/src/main/java/com/gcw/sapienza/places/model/Flag.java
+++ app/src/main/java/com/gcw/sapienza/places/model/Flag.java
@@ -7,7 +7,6 @@ import com.parse.ParseObject;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Class that encapsulate the Post table on Parse.com
@@ -19,8 +18,6 @@ public class Flag extends ParseObject{
      *
      * @return the text content of the post
      */
-
-    public String getObjectId(){ return (String)this.get("objectId"); };
 
     public String getText(){ return (String)this.get("text"); }
 
@@ -40,8 +37,6 @@ public class Flag extends ParseObject{
     public ParseFile getPic() { return (ParseFile)this.get("picture"); }
 
     public String getWeather() { return (String)this.get("weather");  }
-
-    public ArrayList<String> getReports() { return (ArrayList<String>)this.get("reports");  }
 
     public ParseFile getAudio() { return (ParseFile)this.get("audio");  }
 
