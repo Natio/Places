ec7d9321f193b5b8e66d95893178b898c90119b3
michead <countrymetal@live.it>
2015-02-12 02:45:15 +0100

    added basic audio/video playback in FlagFragment

--- app/src/main/java/com/gcw/sapienza/places/FlagFragment.java
+++ app/src/main/java/com/gcw/sapienza/places/FlagFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -33,7 +34,7 @@ import java.net.URL;
 /**
  * Created by mic_head on 02/02/15.
  */
-public class FlagFragment extends Fragment implements View.OnClickListener {
+public class FlagFragment extends Fragment implements View.OnClickListener, View.OnTouchListener {
 
     private String text;
     private String id;
@@ -52,8 +53,10 @@ public class FlagFragment extends Fragment implements View.OnClickListener {
     private EditText flagText;
     private ImageView profilePicimageView;
     private RelativeLayout frameLayout;
-
     private ImageView focused_iw;
+    private ImageView playVideoButton;
+    private FrameLayout videoHolder;
+    private ImageView audioHolder;
 
     private enum MediaType{ PIC, AUDIO, VIDEO, NONE }
     private MediaType mediaType;
@@ -101,34 +104,77 @@ public class FlagFragment extends Fragment implements View.OnClickListener {
     }
 
     @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+        if(mediaPlayer != null) mediaPlayer.release();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         super.onCreateView(inflater, container, savedInstanceState);
 
         view = inflater.inflate(R.layout.flag_layout, container, false);
 
-        iw = ((ImageView)view.findViewById(R.id.pic));
+        iw = (ImageView)view.findViewById(R.id.pic);
         vv = (VideoView)view.findViewById(R.id.vid);
         flagText = (EditText)view.findViewById(R.id.text);
         authorTextView = (TextView)view.findViewById(R.id.author);
         profilePicimageView = (ImageView)view.findViewById(R.id.profile_pic);
         frameLayout = (RelativeLayout)view.findViewById(R.id.frame_layout);
+        playVideoButton = (ImageView)view.findViewById(R.id.play_video_button);
+        videoHolder = (FrameLayout)view.findViewById(R.id.video_holder);
+        audioHolder = (ImageView) view.findViewById(R.id.audio);
 
         iw.setOnClickListener(this);
-        frameLayout.setOnClickListener(this);
+        vv.setOnTouchListener(this);
+        audioHolder.setOnClickListener(this);
+        // playVideoButton.setOnClickListener(this);
 
-        if(mediaType == MediaType.NONE || mediaType == MediaType.AUDIO)
+        if(mediaType == MediaType.NONE)
+        {
+            audioHolder.setVisibility(View.GONE);
+            iw.setVisibility(View.GONE);
+            videoHolder.setVisibility(View.GONE);
+        }
+        else if(mediaType == MediaType.AUDIO)
         {
             iw.setVisibility(View.GONE);
-            vv.setVisibility(View.GONE);
+            videoHolder.setVisibility(View.GONE);
 
-            if(mediaType == MediaType.AUDIO) playRecording(this.audio_path);
+            try {
+                File temp = new File(audio_path);
+
+                mediaPlayer = new MediaPlayer();
+
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp)
+                    {
+                        audioHolder.setImageResource(R.drawable.play_button);
+                    }
+                });
+
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+                FileInputStream inStream = new FileInputStream(temp);
+                mediaPlayer.setDataSource(inStream.getFD());
+
+                mediaPlayer.prepare();
+            }
+            catch (IOException ioe)
+            {
+                ioe.printStackTrace();
+            }
         }
         else if(mediaType == MediaType.PIC)
         {
             Bitmap bm = BitmapFactory.decodeFile(this.pic_path);
 
-            vv.setVisibility(View.GONE);
+            audioHolder.setVisibility(View.GONE);
+            videoHolder.setVisibility(View.GONE);
             iw.setImageBitmap(bm);
 
             focused_iw = (ImageView)view.findViewById(R.id.focused_pic);
@@ -136,8 +182,21 @@ public class FlagFragment extends Fragment implements View.OnClickListener {
         }
         else
         {
+            audioHolder.setVisibility(View.GONE);
             iw.setVisibility(View.GONE);
-            playVideo(video_path);
+
+            Uri videoUri = Uri.parse(video_path);
+            vv.setVideoURI(videoUri);
+            playVideoButton.setVisibility(View.VISIBLE);
+            // playVideo();
+
+            vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+            {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    playVideoButton.setVisibility(View.VISIBLE);
+                }
+            });
         }
 
         flagText.setText(text);
@@ -160,53 +219,49 @@ public class FlagFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v)
     {
-        if(v.getId() == R.id.frame_layout)
-        {
-            frameLayout.setVisibility(View.GONE);
-        }
-        else if(v.getId() == R.id.pic)
-        {
-            frameLayout.setVisibility(View.VISIBLE);
-        }
+        if(v.getId() == R.id.frame_layout) frameLayout.setVisibility(View.GONE);
+        else if(v.getId() == R.id.pic) frameLayout.setVisibility(View.VISIBLE);
+        // else if(v.getId() == playVideoButton.getId()) playVideo();
+        else if(v.getId() == R.id.audio) playRecording();
     }
 
-    private void playRecording(String audio_path)
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
     {
-        try {
-            File temp = new File(audio_path);
+        if(v.getId() == vv.getId() && event.getAction() == MotionEvent.ACTION_DOWN) return playVideo();
 
-            mediaPlayer = new MediaPlayer();
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        return false;
+    }
 
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                @Override
-                public void onCompletion(MediaPlayer mp)
-                {
-                    mp.release();
-                }
-            });
-
-            FileInputStream inStream = new FileInputStream(temp);
-            mediaPlayer.setDataSource(inStream.getFD());
-
-            mediaPlayer.prepare();
+    private void playRecording()
+    {
+        if(mediaPlayer.isPlaying())
+        {
+            mediaPlayer.pause();
+            audioHolder.setImageResource(R.drawable.play_button);
+        }
+        else
+        {
             mediaPlayer.start();
-
-        } catch (IOException ioe)
-        {
-            ioe.printStackTrace();
+            audioHolder.setImageResource(R.drawable.pause_button);
         }
     }
 
-    private void playVideo(String video_path)
+    private boolean playVideo()
     {
-        if(video_path != null)
+        if(vv.isPlaying())
         {
-            Uri videoUri = Uri.parse(video_path);
-            vv.setVideoURI(videoUri);
-            vv.start();
+            vv.pause();
+            playVideoButton.setVisibility(View.VISIBLE);
         }
+        else
+        {
+            // vv.resume();
+            vv.start();
+            playVideoButton.setVisibility(View.GONE);
+        }
+
+        return true;
     }
 
     @Deprecated
