408053346a75930d0e59f3f0ff72d53f5067661d
Daniele Riccardelli <gotikk@hotmail.it>
2015-03-01 04:46:51 +0100

    facebook users now store fbid, improved myprofile fragment to be used for non-current users, bugfixing

--- app/src/main/java/com/gcw/sapienza/places/fragments/MyProfileFragment.java
+++ app/src/main/java/com/gcw/sapienza/places/fragments/MyProfileFragment.java
@@ -3,6 +3,7 @@ package com.gcw.sapienza.places.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +12,25 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.gcw.sapienza.places.R;
+import com.gcw.sapienza.places.models.Flag;
 import com.gcw.sapienza.places.utils.FacebookUtilCallback;
 import com.gcw.sapienza.places.utils.FacebookUtils;
 import com.gcw.sapienza.places.utils.PlacesLoginUtils;
+import com.parse.CountCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 /**
  * Created by snowblack on 2/26/15.
  */
-public class MyProfleFragment extends Fragment {
+public class MyProfileFragment extends Fragment {
+
+    private static final String TAG = "MyProfileFragment";
 
     private ImageView fbPicView;
     private TextView fbNameView;
+    private TextView postsView;
+    private TextView wowedView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,9 @@ public class MyProfleFragment extends Fragment {
 
         fbPicView = (ImageView)view.findViewById(R.id.fbPicView);
         fbNameView = (TextView)view.findViewById(R.id.fbNameView);
+        postsView = (TextView)view.findViewById(R.id.postsView);
+        wowedView = (TextView)view.findViewById(R.id.wowedView);
+
 
         FacebookUtils.getInstance().getFacebookUsernameFromID(PlacesLoginUtils.getInstance().getCurrentUserId(), new FacebookUtilCallback() {
             @Override
@@ -46,6 +58,19 @@ public class MyProfleFragment extends Fragment {
         });
 
         FacebookUtils.getInstance().loadProfilePicIntoImageView(PlacesLoginUtils.getInstance().getCurrentUserId(), fbPicView, PlacesLoginUtils.PicSize.LARGE);
+
+        ParseQuery<Flag> query = ParseQuery.getQuery("Posts");
+        query.whereEqualTo("fbId", PlacesLoginUtils.getInstance().getCurrentUserId());
+        query.countInBackground(new CountCallback() {
+            @Override
+            public void done(int i, ParseException e) {
+                if(e == null) {
+                    postsView.setText("Flags placed: " + i);
+                }else{
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
 
         return view;
     }
